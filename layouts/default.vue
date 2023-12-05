38aa87e88612497a0c34073e709929c73ad1857f
shaheer-haider <shaheerhaider00000@gmail.com>
2023-12-05 22:12:42 +0500

    project base

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col justify-between min-h-screen">
+    <div class="relative max-w-screen-xl mx-auto">
+      <Header />
+      <NuxtPage class="mt-20 bg-blue-50" />
+      <div class="bottom-radial"></div>
+      <div class="top-radial"></div>
+    </div>
+    <Footer />
+  </div>
+</template>
---
+++ tailwind.config.ts
@@ -0,0 +1,39 @@
+export default {
+    theme: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px'
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+        serif: ['Libre Baskerville', 'serif']
+      },
+
+      extend: {
+        colors: {
+          bgColor: '#070C0E',
+          headingColor: '#FFFFFF',
+          paraColor: '#AAAAAA',
+          primary: '#E81C25',
+          stroke: '#640E12',
+          cardColor: '#0E0B0B',
+        }
+      }
+    },
+    content: [
+      './components/**/*.{vue,js,ts}',
+      './layouts/**/*.vue',
+      './pages/*.vue',
+      './pages/**/*.vue',
+      './pages/**/**/*.vue',
+      './pages/**/**/**/*.vue',
+      './composables/*.{js,ts}',
+      './composables/**/*.{js,ts}',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}'
+    ],
+    plugins: [require('@tailwindcss/typography')],
+    cssPath: '~/assets/css/tailwind.css'
+  }
