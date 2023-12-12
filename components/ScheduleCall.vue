75e50b2b909fac83492004f428cea280a5a01d93
shaheer-haider <shaheerhaider00000@gmail.com>
2023-12-12 14:27:59 +0500

    schedule a call fix

--- components/ScheduleCall.vue
+++ components/ScheduleCall.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="space-y-3 text-center">
+  <div class="space-y-3 text-center mt-2">
     <NuxtLink to="/contact" class="btn-primary">Schedule a call</NuxtLink >
     <p class="text-sm">Ready to get started?</p>
   </div>
